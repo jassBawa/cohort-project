95bc1e2abf3b80526288458962d9b9f1ea3a0c21
jasspreetbawa <jaspreetbawa@ryaz.io>
2024-07-07 18:34:02 +0530

    basic auth done

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar";
+
+export default function Home() {
+  const session = useSession();
+  return (
+    <div className="">
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+    </div>
+  );
+}
